6ef1aec68bdaebec97ffa81caf952e2ab784681f
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-12-17 11:15:49 +0000

    トップページ完成、admin側のuser完了

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
+    @new_game_works = GameWork.order(created_at: :desc).limit(3)
+    @new_game_articles = GameArticle.order(created_at: :desc).limit(3)
   end
 
   def about
