853b511428d0df23cd7c2f913967368284e2e231
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-12-22 21:16:10 +0000

    レイアウトを少々

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -25,7 +25,7 @@ class Admin::SessionsController < Devise::SessionsController
     end
   
     def after_sign_out_path_for(resource)
-      new_admin_session_path
+      homes_cioce_path
     end
 
   # If you have extra params to permit, append them to the sanitizer.
