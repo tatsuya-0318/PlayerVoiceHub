d2608f404c515adda6b27d0b43f9d6463c41c572
tatsuya <146906978+tatsuya-0318@users.noreply.github.com>
2023-12-26 07:02:11 +0000

    ゲストユーザー時のユーザー編集ボタンのレイアウトを変えました。

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
   
 
@@ -43,7 +43,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+   protected
+   
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation, :sex, :age])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
